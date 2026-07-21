2f4314982d4234d6cb0a79c094975cca376a5fe2
Uday Patil <udayp9020@gmail.com>
2026-07-22 00:22:09 +0530

    feat: create featured destinations and section title

--- Frontend/client/src/components/FeaturedDestination.jsx
+++ Frontend/client/src/components/FeaturedDestination.jsx
@@ -1,17 +1,31 @@
-import React from 'react';
-import { roomsDummyData } from '../assets/assets';    
-import HotelCard from './HotelCard';
+import React from "react";
+import { roomsDummyData } from "../assets/assets";
+import HotelCard from "./HotelCard";
+import Title from "./Title";
+import { useNavigate } from 'react-router-dom';
 
 const FeaturedDestination = () => {
-  return (
-    <div className='flex flex-col item-center px-6 md:px-16 lg:px-24 bg-slate-50 py-20 '>
-        <div className='flex flex-wrap item-center justify-center gap-6 mt-20'>
-            {roomsDummyData.slice(0, 4).map((room, index) => (
-                <HotelCard key={room._id} room={room} index={index}/>
-            ))}
-        </div>
-    </div> 
-  )
-}
 
-export default FeaturedDestination
+  const Navigate = useNavigate();
+  return (
+    <div className="flex flex-col item-center px-6 md:px-16 lg:px-24 bg-slate-50 py-20 ">
+      <Title
+        title="Featured Destination"
+        subtitle="Discover oue handpicked selection of exceptional properties around the world, offering unparalleled luxury and unforgettable experiences."
+      />
+
+      <div className="flex flex-wrap item-center justify-center gap-6 mt-20">
+        {roomsDummyData.slice(0, 4).map((room, index) => (
+          <HotelCard key={room._id} room={room} index={index} />
+        ))}
+      </div>
+        <button onClick={() => {Navigate('/rooms'); scrollTo(0,0)}}
+         className="my-16 px-4 py-2 text-sm font-medium border border-gray-300 rounded bg-white hover:bg-gray-50 transition-all cursor-pointer">
+          View All Destinations
+        </button>
+
+    </div>
+  );
+};
+
+export default FeaturedDestination;
