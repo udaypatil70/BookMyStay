aa503789cab7789ef9bf3f6ee1f407f5cf693e1b
Uday Patil <udayp9020@gmail.com>
2026-07-23 00:26:21 +0530

    style: update room listing layout and styling

--- Frontend/client/src/pages/AllRooms.jsx
+++ Frontend/client/src/pages/AllRooms.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { assets, roomsDummyData } from "../assets/assets";
+import { assets, facilityIcons, roomsDummyData } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
 import StarRating from "../components/StarRating";
 
@@ -17,7 +17,7 @@ const AllRooms = () => {
         </div>
 
         {roomsDummyData.map((room) => (
-          <div>
+          <div key={room._id} className="flex flex-col md:flex-row items-start py-10 gap-6 border-b border-gray-300 last:pb-30 last:border-0">
             <img
               onClick={() => {
                 navigate(`/room/${room._id}`);
@@ -47,6 +47,15 @@ const AllRooms = () => {
                 <img src={assets.locationIcon} alt="location-icon" />
                 <span>{room.hotel.address}</span>
               </div>
+              {/* Room Amentities */}
+              <div className="flex flex-wrap items-center mt-3 mb-6 gap-4">
+                {room.amenities.map((item, index) => (
+                  <div key={index} className="flex items-center gap-2 px-3 py-2 rounded-lg bg-[#F5F5FF]/70">
+                    <img src={facilityIcons[item]} alt={item} className="w-5 h-5" />
+                    <p className="text-xs">{item}</p>
+                  </div>
+                ))}
+              </div>
             </div>
           </div>
         ))}
@@ -56,5 +65,4 @@ const AllRooms = () => {
     </div>
   );
 };
-
 export default AllRooms;
