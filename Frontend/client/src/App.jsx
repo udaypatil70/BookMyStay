dae9316822e5b07b3bec261597a6ce3796ecce0a
Uday Patil <udayp9020@gmail.com>
2026-07-23 18:05:01 +0530

    feat: create MyBooking Page layout

--- Frontend/client/src/App.jsx
+++ Frontend/client/src/App.jsx
@@ -5,6 +5,7 @@ import Home from './pages/Home'
 import Footer from './components/Footer';
 import AllRooms from './pages/AllRooms';
 import RoomDetails from './pages/RoomDetails';
+import MyBooking from './pages/MyBooking';
 
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/rooms" element={<AllRooms/>}/>
           <Route path="/rooms/:id" element={<RoomDetails />}/>
+          <Route path="/my-bookings" element={<MyBooking />}/>
         </Routes>
       </div>
       <Footer />
