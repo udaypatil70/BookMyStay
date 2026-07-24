4731077c6e7768cd2966b2159832697a34dc4449
Uday Patil <udayp9020@gmail.com>
2026-07-24 18:55:00 +0530

    feat: create AddRoom Img Section

--- Frontend/client/src/pages/HotelOwner/AddRoom.jsx
+++ Frontend/client/src/pages/HotelOwner/AddRoom.jsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import React, { useState } from "react";
+import Title from "../../components/Title";
+import { assets } from "../../assets/assets";
 
 const AddRoom = () => {
-  return (
-    <div>AddRoom</div>
-  )
-}
+  const [images, setImages] = useState({
+    1: null,
+    2: null,
+    3: null,
+    4: null,
+  });
 
-export default AddRoom
+  const [inputs, setInputs] = useState({
+    roomType: "",
+    pricePerNight: 0,
+    amenities: {
+      "free WiFi": false,
+      "free breakfast": false,
+      "Room Service": false,
+      "Mountain View": false,
+      "Pool Access": false,
+    },
+  });
+
+  return (
+    <form>
+      <Title
+        align="left"
+        font="outfit"
+        title="Add Room"
+        subTitle="Fill in the details carefully and accurate room details, pricing, and amenities, to enhance the user booking experience."
+      />
+
+      {/* Upload Area For Images */}
+      <p className="text-gray-800 mt-10">Images</p>
+
+      <div className="grid grid-cols-2 sm:flex gap-4 my-2 flex-wrap">
+        {Object.keys(images).map((key) => (
+          <label htmlFor={`roomImage${key}`} key={key}>
+            <img
+              className="max-h-13 cursor-pointer opacity-80"
+              src={
+                images[key]
+                  ? URL.createObjectURL(images[key])
+                  : assets.uploadArea
+              }
+              alt=""
+            />
+
+            <input
+              type="file"
+              accept="image/*"
+              id={`roomImage${key}`}
+              hidden
+              onChange={(e) =>
+                setImages({
+                  ...images,
+                  [key]: e.target.files[0],
+                })
+              }
+            />
+          </label>
+        ))}
+      </div>
+    </form>
+  );
+};
+
+export default AddRoom;
--- Frontend/client/src/pages/HotelOwner/DashBoard.jsx
+++ Frontend/client/src/pages/HotelOwner/DashBoard.jsx
@@ -55,41 +55,47 @@ const DashBoard = () => {
 
       <div className="w-full max-w-3xl text-left border border-gray-300 rounded-lg max-h-80 overflow-y-scroll">
         <table className="w-full">
-            <thead className="bg-gray-50">
-                <tr>
-                    <th className="py-3 py-4 text-gray-800 font-medium">User Name</th>
-                   <th className="py-3 py-4 text-gray-800 font-medium max-sm:hidden">Room Name</th>
-                   <th className="py-3 py-4 text-gray-800 font-medium text-center">Total Amount</th>
-                   <th className="py-3 py-4 text-gray-800 font-medium text-center">Payment Status</th>
-                </tr>
-            </thead>
+          <thead className="bg-gray-50">
+            <tr>
+              <th className="py-3 py-4 text-gray-800 font-medium">User Name</th>
+              <th className="py-3 py-4 text-gray-800 font-medium max-sm:hidden">
+                Room Name
+              </th>
+              <th className="py-3 py-4 text-gray-800 font-medium text-center">
+                Total Amount
+              </th>
+              <th className="py-3 py-4 text-gray-800 font-medium text-center">
+                Payment Status
+              </th>
+            </tr>
+          </thead>
 
-            <tbody className="text-sm">
-                {dashBordData.bookings.map((item, index) => (
-                    <tr key={index}>
-                        <td className="py-3 px-4 text-gray-700 border-t border-gray-300">
-                            {item.user.username}
-                        </td>
+          <tbody className="text-sm">
+            {dashBordData.bookings.map((item, index) => (
+              <tr key={index}>
+                <td className="py-3 px-4 text-gray-700 border-t border-gray-300">
+                  {item.user.username}
+                </td>
 
-                        <td className="py-3 px-4 text-gray-700 border-t border-gray-300 mx-sm:hidden">
-                            {item.room.roomType}
-                        </td>
+                <td className="py-3 px-4 text-gray-700 border-t border-gray-300 mx-sm:hidden">
+                  {item.room.roomType}
+                </td>
 
-                        <td className="py-3 px-4 text-gray-700 border-t border-gray-300 text-center">
-                           $ {item.totalPrice}
-                        </td>
+                <td className="py-3 px-4 text-gray-700 border-t border-gray-300 text-center">
+                  $ {item.totalPrice}
+                </td>
 
-                        <td className="py-3 px-4 border-t border-gray-300 flex">
-                                <button className={`py-1 px-4 text-xs rounded-full mx-auto ${item.isPaid ? "bg-green-200 text-green-600" : "bg-amber-200 text-yellow-600"}`}>
-                                    {item.isPaid ? "Completed" : "Pending"}
-                                </button>
-                        </td>
-                    </tr>
-                ))}
-            </tbody>
-
+                <td className="py-3 px-4 border-t border-gray-300 flex">
+                  <button
+                    className={`py-1 px-4 text-xs rounded-full mx-auto ${item.isPaid ? "bg-green-200 text-green-600" : "bg-amber-200 text-yellow-600"}`}
+                  >
+                    {item.isPaid ? "Completed" : "Pending"}
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
         </table>
-
       </div>
     </div>
   );
