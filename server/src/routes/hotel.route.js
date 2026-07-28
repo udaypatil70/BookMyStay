7da2f2cfe0ec69964bdb21600acb9bf7601b3703
Uday Patil <udayp9020@gmail.com>
2026-07-28 18:52:59 +0530

    feat(hotel): implement hotel registration and fix routing issues

--- server/src/routes/hotel.route.js
+++ server/src/routes/hotel.route.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { protect } from "../middleware/auth.middleware.js";
 import { registerHotel } from "../controllers/hotel.controllers.js";
 
 const hotelRouter = express.Router();
--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,10 +1,13 @@
 import express from "express";
 import { protect } from "../middleware/auth.middleware.js";
-import { getUserData , storeRecentSearchedCities} from "../controllers/user.controllers.js";
+import {
+  getUserData,
+  storeRecentSearchedCities,
+} from "../controllers/user.controllers.js";
 
 const userRouter = express.Router();
 
-router.get("/", protect, getUserData);
-router.post("/store-recent-search", protect, storeRecentSearchedCities);
+userRouter.get("/", protect, getUserData);
+userRouter.post("/store-recent-search", protect, storeRecentSearchedCities);
 
 export default userRouter;
