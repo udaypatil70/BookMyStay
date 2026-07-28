4c67da71c523fb5f8910103c9f97642103908174
Uday Patil <udayp9020@gmail.com>
2026-07-28 19:01:22 +0530

    feat(room): create room schema for hotel booking

---
+++ server/src/models/Room.model.js
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+
+const roomSchema = new mongoose.Schema(
+  {
+    hotel: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Hotel",
+      required: true,
+    },
+    roomType: {
+      type: String,
+      required: true,
+    },
+    pricePerNight: {
+      type: Number,
+      required: true,
+    },
+    amenities: {
+      type: [String],
+      required: true,
+    },
+    images: [
+      {
+        type: String,
+      },
+    ],
+    isAvailable: {
+      type: Boolean,
+      default: true,
+    },
+    capacity: {
+      type: Number,
+      required: true,
+    },
+
+    bedType: {
+      type: String,
+      required: true,
+    },
+
+    description: {
+      type: String,
+      default: "",
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Room = mongoose.model("Room", roomSchema);
+
+export default Room;
