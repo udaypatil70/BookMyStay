501219f14d94e50356f1a8ff946587c9e267e9ff
Uday Patil <udayp9020@gmail.com>
2026-07-29 19:16:57 +0530

    feat: revert owner dashboard background change

--- Frontend/client/src/pages/HotelOwner/Layout.jsx
+++ Frontend/client/src/pages/HotelOwner/Layout.jsx
@@ -5,11 +5,11 @@ import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
-    <div className="flex flex-col h-screen">
+    <div className="flex flex-col min-h-screen bg-slate-50">
       <Navbar />
-      <div className="flex h-full">
+      <div className="flex flex-1">
         <Sidebar />
-        <div className="flex-1 p-4 pt-10 md:px-10 h-full">
+        <div className="flex-1 p-4 md:p-8">
           <Outlet />
         </div>
       </div>
